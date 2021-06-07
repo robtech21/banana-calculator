ed9fc00098d9c6e185469580d4101b20f177ae9d
donnerkebab1730 <80350015+donnerkebab1730@users.noreply.github.com>
2021-06-07 11:53:36 +0100

    Create bannana calc.py

---
+++ bannana calc.py
@@ -0,0 +1,14 @@
+
+d = true
+while d == true:
+  a = input("enter number 1")
+  b = input("enter what you want to do:,*,/,+ or -")
+  c = input("enter second number")
+  if 
+d = true
+while d == true:
+  a = int(input("enter number 1"))
+  b = input("enter what you want to do:,*,/,+ or -")
+  c = int(input("enter second number"))
+  if b == "/":
+      a/b
